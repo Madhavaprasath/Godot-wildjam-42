1f1d90f07fabb166ad627717d1d3687e7474cae5
Sylphidius <xavier.bounet@outlook.fr>
2022-04-11 19:17:08 +0200

    player state machine handles dash and getting hit

--- Fragile/Entity/Entity.gd
+++ Fragile/Entity/Entity.gd
@@ -4,7 +4,6 @@ enum states{
 	IDLE,MOVEMENT,ATTACK,DASH,STUN
 }
 var current_state=states.IDLE
-var stun_duration = 0
 var types = ["player","enemy"]
 var type = types[0]
 var myAttack
@@ -15,13 +14,22 @@ func _process(delta):
 		queue_free()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$StunTime.connect("timeout",self,"_on_StunTime_timeout")
+	pass
 
 func get_hit(attack ):
 	hp = hp - attack.damage
 	current_state = states.STUN
-	stun_duration = attack.stun
+	$StunTime.wait_time = attack.stun
+	$StunTime.start();
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
+
+func _on_StunTime_timeout():
+	current_state = states.IDLE
